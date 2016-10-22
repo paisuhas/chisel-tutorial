703f2c694420320ce803d50ad4b5baa3a1ba0550
Suhas Rohit Pai <spai@cs.wisc.edu>
2016-10-22 12:17:25 -0500

    Solution to Max2 testbench problem

--- problems/Max2.scala
+++ problems/Max2.scala
@@ -14,10 +14,12 @@ class Max2 extends Module {
 class Max2Tests(c: Max2) extends Tester(c) {
   for (i <- 0 until 10) {
     // FILL THIS IN HERE
-    poke(c.io.in0, 0)
-    poke(c.io.in1, 0)
+    val in0 = rnd.nextInt(1 << 8)
+    val in1 = rnd.nextInt(1 << 8)
+    poke(c.io.in0, in0)
+    poke(c.io.in1, in1)
     // FILL THIS IN HERE
     step(1)
-    expect(c.io.out, 1)
+    expect(c.io.out, if (in0 > in1) in0 else in1)
   }
 }
